6a2b1423142ae7765aab84bfcc6985bdd0588d8d
giovanabritooliveira <giovana.oliveira@ccc.ufcg.edu.br>
2020-10-16 14:44:13 -0300

    exemplo 1

---
+++ exemplos/exemplo1.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+primeiro_nome = input("Digite seu primeiro nome: ")
+ultimo_nome = input("Digite seu último nome: ")
+idade = input("Digite sua idade: ")
+print("Bem vindo " + primeiro_nome + " " + ultimo_nome + " de " + idade + " anos!")
